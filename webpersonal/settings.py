1404a1f1e7445eaee55729c339087187de8e7b60
María Victoria Bastardo <maria15victoria1998@gmail.com>
2024-11-28 15:07:59 -0400

    add decouple

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y^su48(0q##(jay8(p$20ffc3qiqmw50xluyth(82t7@85hhmo'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
